20980c3c4749e004f80c11e4379f52ca4ac06be6
leoniewe33 <130054748+leoniewe33@users.noreply.github.com>
2024-12-08 20:32:14 +0100

    Verlinkung

--- lehrer_startseite.php
+++ lehrer_startseite.php
@@ -66,7 +66,7 @@ if (isset($_SESSION["user_id"])) {
 
         <div class="layout-drawer" id="drawer">
             <nav class="navigation">
-                <a class="navigation-link" href="startseite.php">Startseite</a>
+                <a class="navigation-link" href="lehrer_startseite.php">Startseite</a>
                 <a class="navigation-link" href="">Suche</a>
                 <a class="navigation-link" href="tätigKatalog.php">Tätigkeitenkatalog</a>
                 <a class="navigation-link" href="">Profil</a>
